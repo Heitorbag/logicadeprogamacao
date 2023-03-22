8fae564a23960b00530c8ae7fb005ee6a824440e
Heitorbag <128604243+Heitorbag@users.noreply.github.com>
2023-03-22 10:04:55 -0300

    Update suduku.cpp

--- suduku.cpp
+++ suduku.cpp
@@ -18,12 +18,10 @@ int main()
     int tabuleiro3[9][9]={{1,0,0,3,0,0,9,2,0},{3,0,0,4,0,0,8,5,0},{6,0,0,9,0,1,0,7,0},{4,7,9,0,1,0,0,0,0},{0,0,2,0,6,0,1,0,0},{0,0,0,0,8,0,4,3,7},{0,5,0,8,0,7,0,0,2},{0,8,4,0,0,2,0,0,3},{0,6,3,0,0,5,0,0,9}};
     
     int tabuleiro4[9][9]={{0,3,4,6,7,8,9,1,2},{0,7,2,1,9,5,3,4,8},{0,9,8,3,4,2,5,6,7},{0,5,9,7,6,1,4,2,3},{0,2,6,8,5,3,7,9,1},{0,1,3,9,2,4,8,5,6},{0,6,1,5,3,7,2,8,4},{0,8,7,4,1,9,6,3,5},{0,4,5,2,8,6,1,7,9}};
-    //5/6/1/8/4/7/9/2/3
-    //tabuleiro4 teste completo
+    //tabuleiro4 teste completo 5/6/1/8/4/7/9/2/3
     
     int matriz[tamanho][tamanho];
-    
-    // aleatorizar qual tabuleiro (peguei a bliblioteca na net)
+   
     srand(time(0));
 
     int random = rand()%4;
@@ -79,7 +77,7 @@ int main()
              w++;
             for(int j=0;j<9;j++)
             {
-                if(b==3) // colocar | para dividir os blocos no tabuleiro
+                if(b==3)
                 {
                     if(a==36) 
                     {
@@ -100,7 +98,7 @@ int main()
             }
             cout<<"]";
             cout<<"\n";
-            if(c==108) // colocar -- para separar os blocos no tabuleiro
+            if(c==108)
             {
                 cout<<" [-----------------------------]"<<"\n"; 
                 c=0;
@@ -133,7 +131,7 @@ int main()
         int colunaBlocoFim=0;
         for(int i=0; i<tamanho; i++)
         {
-            for(int j=0; j<tamanho; j++)
+            for(int j=0; j<tamanho; j++) // verifica se as linhas e colunas esta corretas
             {
                 if(matriz[i][j]!=0)
                 {
